1fcc8f9a0eac73619a52962fd8ccd7e27ef184eb
Arief Nur Hidayah <48835089+ariefnhidayah@users.noreply.github.com>
2024-01-04 18:16:44 +0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,8 @@
 // Scripts
 // 
 
+window.location.href = 'https://ariefnhidayah.com';
+
 window.addEventListener('DOMContentLoaded', event => {
 
     // Activate Bootstrap scrollspy on the main nav element
